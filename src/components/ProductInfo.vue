499978c90d46429d7fce08c00b28862719b89528
Shangwei <Shangwei.Huang@telexpress.com>
2018-12-04 16:47:02 +0800

    ProductInfo user input.

--- src/components/ProductInfo.vue
+++ src/components/ProductInfo.vue
@@ -10,6 +10,13 @@
         <div class="product-content">{{ product.desc }}</div>
         <span class="product-price">{{ product.price | currency }}</span>
       </div>
+      <div class="purchase-form">
+        <div class="form-control">
+          <label for="qty">Qty: </label>
+          <input type="number" id="qty" v-model="quantity">
+        </div>
+        <div class="purchase-amount">{{ amount | currency }}</div>
+      </div>
     </div>
   </div>
 </template>
@@ -19,18 +26,26 @@ export default {
   name: 'ProductInfo',
   props: {
     item: {
-      Object,
-      default: {},
+      type: Object,
+      default() {
+        return {};
+      },
     },
   },
   data() {
     return {
       product: this.item,
+      quantity: 0,
     };
   },
   watch: {
     item(value) {
       this.product = value;
+    },
+  },
+  computed: {
+    amount() {
+      return this.product.price * this.quantity;
     },
   },
 };
@@ -83,5 +98,45 @@ export default {
   color: rgba(255, 30, 0, 0.884);
   font-size: 2em;
   float: right;
+}
+
+.purchase-form {
+  display: flex;
+  flex-wrap: nowrap;
+  width: 100%;
+  justify-content: center;
+  align-items: baseline;
+}
+
+.form-control {
+  display: flex;
+  justify-content: flex-end;
+  width: 100%;
+  padding: 10px;
+}
+
+.purchase-amount {
+  display: flex;
+  justify-content: flex-start;
+  width: 100%;
+}
+
+.form-control label {
+  font-size: 1.2em;
+}
+
+.form-control input::-webkit-inner-spin-button{
+  -webkit-appearance: none;
+}
+
+.form-control input {
+  margin: 0 5px;
+  border: 1px solid #ddd;
+  box-sizing: border-box;
+  border-radius: 2px;
+  line-height: 20px;
+  width: 40px;
+  font-size: 1.1em;
+  text-align: center;
 }
 </style>
