fc0b2804b1ad2a0949bc2272360df02db2f622c1
Shangwei <Shangwei.Huang@telexpress.com>
2018-12-06 18:22:03 +0800

    Add environment settings.

--- src/services/product-service.js
+++ src/services/product-service.js
@@ -1,9 +1,9 @@
 import { ajax } from 'rxjs/ajax';
 import { catchError } from 'rxjs/operators';
 
-const sourceUrl = 'http://localhost:3000/products';
+const sourceUrl = `${process.env.VUE_APP_API_BASE_URL}/products`;
 const headers = {
-  'Access-Control-Allow-Origin': 'http://localhost:3000',
+  'Access-Control-Allow-Origin': `${process.env.VUE_APP_API_BASE_URL}`,
 };
 
 export default {
